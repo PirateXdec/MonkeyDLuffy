86325f13ad7f8dadd6f605a5972de424120bf08d
PR1NC3C0D3 <75202727+PR1NC3C0D3@users.noreply.github.com>
2021-04-21 20:38:37 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -237,7 +237,8 @@ def start(update: Update, context: CallbackContext):
                         ],
                         [
                             InlineKeyboardButton(
-                                text="👒 New in the Crew ?", callback_data="help_back",
+                                text="👒 New in the Crew ?", 
+                                callback_data="help_back",
 
                             )
                       
