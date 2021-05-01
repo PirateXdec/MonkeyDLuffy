0dfbfdadba2eb2d25467db074b79ec22d52a4dad
Dojeto <81398258+Dojeto@users.noreply.github.com>
2021-05-01 20:13:02 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -19,6 +19,7 @@ from SaitamaRobot import (
     StartTime,
     telethn,
     updater,
+    pbot, 
 )
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
@@ -590,4 +591,5 @@ def main():
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
+    pbot.start() 
     main()
