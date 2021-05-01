d12778a205ab6e4fac24a3ec449df76c91e9ab0e
Dojeto <81398258+Dojeto@users.noreply.github.com>
2021-05-01 20:05:00 +0530

    Update dbfunctions.py

--- SaitamaRobot/utils/dbfunctions.py
+++ SaitamaRobot/utils/dbfunctions.py
@@ -2,6 +2,7 @@ from SaitamaRobot import db
 from typing import Dict, List, Union
 
 karmadb = db.karma
+coupledb=db.couple
 
 async def get_karmas_count() -> dict:
     chats = karmadb.find({"chat_id": {"$lt": 0}})
@@ -48,6 +49,37 @@ async def update_karma(chat_id: int, name: str, karma: dict):
         },
         upsert=True
     )
+
+async def _get_lovers(chat_id: int):
+    lovers = await coupledb.find_one({"chat_id": chat_id})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    else:
+        return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "couple": lovers
+            }
+        },
+        upsert=True
+    )
+
 async def int_to_alpha(user_id: int) -> str:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     text = ""
