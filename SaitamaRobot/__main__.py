d6b804827d5fde85666e46586db3eeaadf1dac62
PR1NC3C0D3 <75202727+PR1NC3C0D3@users.noreply.github.com>
2021-05-25 10:30:17 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -218,7 +218,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
           update.effective_message.reply_video(
-                LUFFY_VID, caption=f"Hmmm , I am gonna be the king of the pirates"
+                LUFFY_VID, caption=f"I Am Gonna Be The King Of The Pirates"
         )
            
 
