9419129a14098bb264f7753a08f891e2eb373e70
PR1NC3C0D3 <75202727+PR1NC3C0D3@users.noreply.github.com>
2021-06-01 21:53:57 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/MonkeyDLuffyUpdates/6">?</a>]'.format(
+        text += ' [<a href="https://t.me/BotProtocol/11">?</a>]'.format(
             bot.username
         )
 
