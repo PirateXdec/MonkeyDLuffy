556b3e27bdee3d95ed076352e4a6ed0b737b4744
PR1NC3C0D3 <75202727+PR1NC3C0D3@users.noreply.github.com>
2021-06-12 18:35:37 +0530

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -454,6 +454,26 @@ __help__ = """
 • `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
 • `/kang`*:* reply to a sticker to add it to your pack.
 • `/stickers`*:* Find stickers for given term on combot sticker catalogue
+
+Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
+
+*NOTE:* Blacklist stickers do not affect the group admin
+
+ • `/blsticker`*:* See current blacklisted sticker
+
+*Only admin:*
+
+ • `/addblsticker <sticker link>`*:* Add the sticker trigger to the black list. Can be added via reply sticker
+
+ • `/unblsticker <sticker link>`*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
+
+ • `/rmblsticker <sticker link>`*:* Same as above
+
+ • `/blstickermode <ban/tban/mute/tmute>`*:* sets up a default action on what to do if users use blacklisted stickers
+
+Note:
+
+ • `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 """
 
 __mod_name__ = "Stickers"
