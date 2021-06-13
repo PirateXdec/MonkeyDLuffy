d6050ac477b3780a8e60738976778595c69e78f8
PR1NC3C0D3 <75202727+PR1NC3C0D3@users.noreply.github.com>
2021-06-13 15:08:46 +0530

    Update google.py

--- SaitamaRobot/modules/google.py
+++ SaitamaRobot/modules/google.py
@@ -277,10 +277,3 @@ async def apk(e):
 
 __mod_name__ = "GOOGLE"
 
-__help__ = """
- ✶ /google <text>*:* Perform a google search
- ✶ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
- ✶ /app <appname>*:* Searches for an app in Play Store and returns its details.
- ✶ /reverse: Does a reverse image search of the media which it was replied to.
-
-"""
